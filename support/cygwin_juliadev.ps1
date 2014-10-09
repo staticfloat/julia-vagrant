b553ea1ba8e4348f4b4f63b3a34fdc91896271a2
Tony Kelman <tony@kelman.net>
2014-10-09 06:06:37 -0700

    adjust cygwin package installation

--- support/cygwin_juliadev.ps1
+++ support/cygwin_juliadev.ps1
@@ -1,12 +1,13 @@
+mkdir -Force C:\cygwin | Out-Null
 (new-object net.webclient).DownloadFile(
   "http://cygwin.com/setup-x86_64.exe", "C:\cygwin\setup-x86_64.exe")
-foreach ($pkg in @("make","curl","patch","python","gcc-g++","m4","p7zip",
-    "mingw64-i686-gcc-g++","mingw64-i686-gcc-fortran",
-    "mingw64-x86_64-gcc-g++","mingw64-x86_64-gcc-fortran")) {
-  Write-Host $("Installing "+$pkg)
-  & C:\cygwin\setup-x86_64.exe -q -n -g -P $pkg >$null 2>&1
+foreach ($pkg in @("git,make,curl,patch,python,gcc-g++,m4,p7zip",
+    "mingw64-i686-gcc-g++,mingw64-i686-gcc-fortran",
+    "mingw64-x86_64-gcc-g++,mingw64-x86_64-gcc-fortran")) {
+  C:\cygwin\setup-x86_64.exe -q -n -R C:\cygwin -l C:\cygwin\packages `
+    -s http://mirror.mcs.anl.gov/cygwin -g -P $pkg | Where-Object `
+    -FilterScript {$_ -notlike "Installing file *"} | Write-Output
 }
-C:\cygwin\bin\sh -lc "git config --global core.autocrlf input"
 C:\cygwin\bin\sh -lc "if ! [ -e julia32 ]; then git clone \
   git://github.com/JuliaLang/julia.git julia32; fi && \
   cd julia32 && git submodule init && git pull && \
