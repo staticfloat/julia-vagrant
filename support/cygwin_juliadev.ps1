3985de2377f2bde17f85466383a845def4a918bd
Tony Kelman <tony@kelman.net>
2014-10-11 06:49:25 -0700

    do all the other build steps too for good measure

--- support/cygwin_juliadev.ps1
+++ support/cygwin_juliadev.ps1
@@ -9,10 +9,12 @@ foreach ($pkg in @("git,make,curl,patch,python,gcc-g++,m4,p7zip",
     -FilterScript {$_ -notlike "Installing file *"} | Write-Output
 }
 C:\cygwin\bin\sh -lc "if ! [ -e julia32 ]; then git clone \
-  git://github.com/JuliaLang/julia.git julia32; fi && \
-  cd julia32 && git submodule init && git pull && \
-  git submodule update && XC_HOST=i686-w64-mingw32 make -C deps"
+  git://github.com/JuliaLang/julia.git julia32; fi && cd julia32 && \
+  git submodule init && git pull && git submodule update && \
+  echo 'XC_HOST = i686-w64-mingw32' > Make.user && make cleanall && \
+  make -j2 test && make win-extras && make dist"
 C:\cygwin\bin\sh -lc "if ! [ -e julia64 ]; then git clone \
-  git://github.com/JuliaLang/julia.git julia64; fi && \
-  cd julia64 && git submodule init && git pull && \
-  git submodule update && XC_HOST=x86_64-w64-mingw32 make -C deps"
+  git://github.com/JuliaLang/julia.git julia64; fi && cd julia64 && \
+  git submodule init && git pull && git submodule update && \
+  echo 'XC_HOST=x86_64-w64-mingw32' > Make.user && make cleanall && \
+  make -j2 test && make win-extras && make dist"
