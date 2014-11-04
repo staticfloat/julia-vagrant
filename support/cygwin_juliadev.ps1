ac5f5fb933a7f21385c74bddc17383d690eb4e50
Tony Kelman <tony@kelman.net>
2014-11-03 23:51:39 -0800

    install cmake

--- support/cygwin_juliadev.ps1
+++ support/cygwin_juliadev.ps1
@@ -1,7 +1,7 @@
 mkdir -Force C:\cygwin | Out-Null
 (new-object net.webclient).DownloadFile(
   "http://cygwin.com/setup-x86_64.exe", "C:\cygwin\setup-x86_64.exe")
-foreach ($pkg in @("git,make,curl,patch,python,gcc-g++,m4,p7zip",
+foreach ($pkg in @("git,make,curl,patch,python,gcc-g++,m4,cmake,p7zip",
     "mingw64-i686-gcc-g++,mingw64-i686-gcc-fortran",
     "mingw64-x86_64-gcc-g++,mingw64-x86_64-gcc-fortran")) {
   C:\cygwin\setup-x86_64.exe -q -n -R C:\cygwin -l C:\cygwin\packages `
@@ -16,5 +16,5 @@ C:\cygwin\bin\sh -lc "if ! [ -e julia32 ]; then git clone \
 C:\cygwin\bin\sh -lc "if ! [ -e julia64 ]; then git clone \
   git://github.com/JuliaLang/julia.git julia64; fi && cd julia64 && \
   git submodule init && git pull && git submodule update && \
-  echo 'XC_HOST=x86_64-w64-mingw32' > Make.user && make cleanall && \
+  echo 'XC_HOST = x86_64-w64-mingw32' > Make.user && make cleanall && \
   make -j2 test && make win-extras && make dist"
