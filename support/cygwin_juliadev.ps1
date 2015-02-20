60443bad5aa0a470513d13a5ba653dc69f8e33f6
Tony Kelman <tony@kelman.net>
2015-02-20 00:57:47 -0800

    change cygwin mirror (budget cuts?)
    and set HOMEDRIVE and HOMEPATH, because windows

--- support/cygwin_juliadev.ps1
+++ support/cygwin_juliadev.ps1
@@ -5,9 +5,11 @@ foreach ($pkg in @("git,make,curl,patch,python,gcc-g++,m4,cmake,p7zip",
     "mingw64-i686-gcc-g++,mingw64-i686-gcc-fortran",
     "mingw64-x86_64-gcc-g++,mingw64-x86_64-gcc-fortran")) {
   C:\cygwin\setup-x86_64.exe -q -n -R C:\cygwin -l C:\cygwin\packages `
-    -s http://mirror.mcs.anl.gov/cygwin -g -P $pkg | Where-Object `
+    -s http://mirrors.chauf.net/cygwin -g -P $pkg | Where-Object `
     -FilterScript {$_ -notlike "Installing file *"} | Write-Output
 }
+[Environment]::SetEnvironmentVariable("HOMEDRIVE", "C:")
+[Environment]::SetEnvironmentVariable("HOMEPATH", "\Users\vagrant")
 C:\cygwin\bin\sh -lc "if ! [ -e julia32 ]; then git clone \
   git://github.com/JuliaLang/julia.git julia32; fi && cd julia32 && \
   git submodule init && git pull && git submodule update && \
