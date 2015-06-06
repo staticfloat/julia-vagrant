24c1ef41202e65bb3a98baa141aa9a9c3bcd5ad5
Tony Kelman <tony@kelman.net>
2015-06-06 09:52:24 -0700

    Update cygwin provisioning script

--- support/cygwin_juliadev.ps1
+++ support/cygwin_juliadev.ps1
@@ -5,18 +5,16 @@ foreach ($pkg in @("git,make,curl,patch,python,gcc-g++,m4,cmake,p7zip",
     "mingw64-i686-gcc-g++,mingw64-i686-gcc-fortran",
     "mingw64-x86_64-gcc-g++,mingw64-x86_64-gcc-fortran")) {
   C:\cygwin\setup-x86_64.exe -q -n -R C:\cygwin -l C:\cygwin\packages `
-    -s http://mirrors.chauf.net/cygwin -g -P $pkg | Where-Object `
+    -s http://mirrors.mit.edu/cygwin -g -P $pkg | Where-Object `
     -FilterScript {$_ -notlike "Installing file *"} | Write-Output
 }
 [Environment]::SetEnvironmentVariable("HOMEDRIVE", "C:")
 [Environment]::SetEnvironmentVariable("HOMEPATH", "\Users\vagrant")
 C:\cygwin\bin\sh -lc "if ! [ -e julia32 ]; then git clone \
-  git://github.com/JuliaLang/julia.git julia32; fi && cd julia32 && \
-  git submodule init && git pull && git submodule update && \
+  git://github.com/JuliaLang/julia.git julia32; fi && cd julia32 && git pull && \
   echo 'XC_HOST = i686-w64-mingw32' > Make.user && make cleanall && \
-  make -j2 test && make win-extras && make dist"
+  make -j2 test && make win-extras && make binary-dist"
 C:\cygwin\bin\sh -lc "if ! [ -e julia64 ]; then git clone \
-  git://github.com/JuliaLang/julia.git julia64; fi && cd julia64 && \
-  git submodule init && git pull && git submodule update && \
+  git://github.com/JuliaLang/julia.git julia64; fi && cd julia64 && git pull && \
   echo 'XC_HOST = x86_64-w64-mingw32' > Make.user && make cleanall && \
-  make -j2 test && make win-extras && make dist"
+  make -j2 test && make win-extras && make binary-dist"
